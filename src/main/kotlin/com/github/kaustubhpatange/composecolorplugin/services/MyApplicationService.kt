c6494bf5cf1149fe9631a724ad1f9f899431ac18
GitHub Action <action@github.com>
2020-11-23 17:53:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kaustubhpatange/composecolorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kaustubhpatange.composecolorplugin.services
+
+import com.github.kaustubhpatange.composecolorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kaustubhpatange/composecolorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kaustubhpatange/composecolorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kaustubhpatange.composecolorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kaustubhpatange.composecolorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
